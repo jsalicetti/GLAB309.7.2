aee6a3358495bc1d5596160f9f4ca0614d8fc245
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-17 09:41:35 -0400

    complete

---
+++ lab/src/main/java/model/Student.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+public class Student {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    @Column(unique = true)
+    private Long accountNo;
+    private Long marks;
+}
+
